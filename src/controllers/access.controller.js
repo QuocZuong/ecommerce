d98e35a6165d9aee8afc83583d36d2708a78687d
Zuong <quocvuongle.ct@gmail.com>
2023-12-07 21:35:32 +0700

    create ErrorResponse class

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -2,12 +2,8 @@
 import AccessService from "../services/access.service.js";
 class AccessController {
     signUp = async (req, res, next) => {
-        try {
-            console.log(`[P]::signUp::`, req.body);
-            return res.status(201).json(await AccessService.signUp(req.body));
-        } catch (error) {
-            next(error);
-        }
+        console.log(`[P]::signUp::`, req.body);
+        return res.status(201).json(await AccessService.signUp(req.body));
     };
 }
 
