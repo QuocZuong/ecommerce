b82fe36e78de5e0fcdfc72559642b47b51fa68b3
Zuong <quocvuongle.ct@gmail.com>
2023-12-08 11:02:06 +0700

    apply success class to controller

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,9 +1,10 @@
 "use strict";
 import AccessService from "../services/access.service.js";
+import { Created } from "../core/success.response.js";
 class AccessController {
     signUp = async (req, res, next) => {
-        console.log(`[P]::signUp::`, req.body);
-        return res.status(201).json(await AccessService.signUp(req.body));
+        new Created({ message: "Sign up successfully", metadata: await AccessService.signUp(req.body) }).send(res);
+        // return res.status(201).json(await AccessService.signUp(req.body));
     };
 }
 
