d9fae364d0078559ed31232f616564d583566d6b
Zuong <quocvuongle.ct@gmail.com>
2023-12-08 22:41:04 +0700

    update create KeyToken

--- src/services/keyToken.service.js
+++ src/services/keyToken.service.js
@@ -6,14 +6,25 @@ import keyToken from "../models/keytoken.model.js";
 creating a key token using a user ID and public key + store it in database. */
 
 export class KeyTokenService {
-    static createKeyToken = async ({ userId, publicKey, privateKey }) => {
+    static createKeyToken = async ({ userId, publicKey, privateKey, refreshToken }) => {
         try {
             // convert public key from object to string to store in database
-            const tokens = await keyToken.create({
-                user: userId,
-                publicKey,
-                privateKey,
-            });
+            // const tokens = await keyToken.create({
+            //     user: userId,
+            //     publicKey,
+            //     privateKey,
+            // });
+
+            // return tokens ? tokens.publicKey : null;
+            const filter = { user: userId },
+                update = {
+                    publicKey,
+                    privateKey,
+                    refreshTokensUsed: [],
+                    refreshToken,
+                },
+                options = { upsert: true, new: true };
+            const tokens = await keyToken.findOneAndUpdate(filter, update, options);
 
             return tokens ? tokens.publicKey : null;
         } catch (error) {
