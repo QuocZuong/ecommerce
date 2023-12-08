2c4d77e04f5c61690b6030ff202d16503cc75f87
Zuong <quocvuongle.ct@gmail.com>
2023-12-08 22:39:56 +0700

    create new error response

--- src/core/error.response.js
+++ src/core/error.response.js
@@ -1,14 +1,6 @@
 "use strict";
 
-const STATUS_CODE = {
-    FORBIDDEN: 403,
-    CONFLICT: 409,
-};
-
-const REASON_STATUS_CODE = {
-    FORBIDDEN: "Bad request error",
-    CONFLICT: "Conflict error",
-};
+import httpStatusCode from "../utils/httpStatusCode.js";
 
 class ErrorResponse extends Error {
     constructor(message, status) {
@@ -18,15 +10,24 @@ class ErrorResponse extends Error {
 }
 
 class ConflictRequestError extends ErrorResponse {
-    constructor(message = REASON_STATUS_CODE.CONFLICT, statusCode = STATUS_CODE.CONFLICT) {
+    constructor(message = httpStatusCode.ReasonPhrases.CONFLICT, statusCode = httpStatusCode.StatusCodes.CONFLICT) {
         super(message, statusCode);
     }
 }
 
 class BadRequestError extends ErrorResponse {
-    constructor(message = REASON_STATUS_CODE.FORBIDDEN, statusCode = STATUS_CODE.FORBIDDEN) {
+    constructor(message = httpStatusCode.ReasonPhrases.FORBIDDEN, statusCode = httpStatusCode.StatusCodes.FORBIDDEN) {
         super(message, statusCode);
     }
 }
 
-export { BadRequestError, ConflictRequestError };
+class AuthFailureError extends ErrorResponse {
+    constructor(
+        message = httpStatusCode.ReasonPhrases.UNAUTHORIZED,
+        statusCode = httpStatusCode.StatusCodes.UNAUTHORIZED,
+    ) {
+        super(message, statusCode);
+    }
+}
+
+export { BadRequestError, ConflictRequestError, AuthFailureError };
