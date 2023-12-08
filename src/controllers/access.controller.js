4d8ddf49307c367dcb1efb9fcd3971c1914d3a64
Zuong <quocvuongle.ct@gmail.com>
2023-12-08 22:38:57 +0700

    add login to access controller

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,7 +1,14 @@
 "use strict";
 import AccessService from "../services/access.service.js";
-import { Created } from "../core/success.response.js";
+import { Created, Success } from "../core/success.response.js";
 class AccessController {
+    login = async (req, res, next) => {
+        console.log("AccessController return", await AccessService.login(req.body));
+        new Success({
+            metadata: await AccessService.login(req.body),
+        }).send(res);
+    };
+
     signUp = async (req, res, next) => {
         new Created({ message: "Sign up successfully", metadata: await AccessService.signUp(req.body) }).send(res);
         // return res.status(201).json(await AccessService.signUp(req.body));
