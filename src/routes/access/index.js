aa0235278b0c91312a8babf5399c5d0d0c2c3095
Zuong <quocvuongle.ct@gmail.com>
2023-12-08 22:40:34 +0700

    add new route

--- src/routes/access/index.js
+++ src/routes/access/index.js
@@ -3,9 +3,11 @@
 import express from "express";
 import accessController from "../../controllers/access.controller.js";
 import { asyncHandler } from "../../auth/checkAuth.js";
+
 const router = express.Router();
 
 // signUp
 router.post("/shop/signup", asyncHandler(accessController.signUp));
+router.post("/shop/login", asyncHandler(accessController.login));
 
 export default router;
