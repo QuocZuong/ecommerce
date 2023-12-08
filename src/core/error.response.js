22a01eabe378dd4ec76555cf7f125eb673970262
Zuong <quocvuongle.ct@gmail.com>
2023-12-08 11:02:22 +0700

    change const name

--- src/core/error.response.js
+++ src/core/error.response.js
@@ -1,11 +1,11 @@
 "use strict";
 
-const StatusCode = {
+const STATUS_CODE = {
     FORBIDDEN: 403,
     CONFLICT: 409,
 };
 
-const ReasonStatusCode = {
+const REASON_STATUS_CODE = {
     FORBIDDEN: "Bad request error",
     CONFLICT: "Conflict error",
 };
@@ -18,13 +18,13 @@ class ErrorResponse extends Error {
 }
 
 class ConflictRequestError extends ErrorResponse {
-    constructor(message = ReasonStatusCode.CONFLICT, statusCode = StatusCode.CONFLICT) {
+    constructor(message = REASON_STATUS_CODE.CONFLICT, statusCode = STATUS_CODE.CONFLICT) {
         super(message, statusCode);
     }
 }
 
 class BadRequestError extends ErrorResponse {
-    constructor(message = ReasonStatusCode.FORBIDDEN, statusCode = StatusCode.FORBIDDEN) {
+    constructor(message = REASON_STATUS_CODE.FORBIDDEN, statusCode = STATUS_CODE.FORBIDDEN) {
         super(message, statusCode);
     }
 }
